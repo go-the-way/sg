fd4efd116efe24ff5d443d7b31a518d0140e0bc9
billcoding <wangmiao07@sina.com>
2021-09-01 12:06:35 +0800

    Add test_util.go

---
+++ test_util.go
@@ -0,0 +1,31 @@
+package sgen
+
+import (
+	"reflect"
+	"testing"
+)
+
+type geImpl struct {
+}
+
+func (g *geImpl) SQL() (string, []interface{}) {
+	return "geImpl", nil
+}
+
+func TestSingleEqual(t *testing.T, except, v interface{}) {
+	if !reflect.DeepEqual(except, v) {
+		t.Fatalf("%v except = [%v] but get = [%v]\n", t.Name(), except, v)
+	}
+	t.Logf("%v except = [%v] get get = [%v]\n", t.Name(), except, v)
+}
+
+func TestEqual(t *testing.T, getSql, exceptSql string, getPs, exceptPs []interface{}) {
+	if getSql != exceptSql {
+		t.Fatalf("%v except sql = [%v] but get sql = [%v]\n", t.Name(), exceptSql, getSql)
+	}
+	if !reflect.DeepEqual(getPs, exceptPs) {
+		t.Fatalf("%v except ps = [%v] but get ps = [%v]\n", t.Name(), exceptPs, getPs)
+	}
+	t.Logf("%v except sql = [%v] get sql = [%v]\n", t.Name(), exceptSql, getSql)
+	t.Logf("%v except ps = [%v] get ps = [%v]\n", t.Name(), exceptPs, getPs)
+}
