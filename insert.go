a724e0345569cec43f97dd5f838fccb0fa6db862
billcoding <wangmiao07@sina.com>
2021-09-02 14:09:24 +0800

    Update: insert

--- insert.go
+++ insert.go
@@ -7,5 +7,56 @@ var (
 			" ", "", "", false)
 	}
 
-	Values = func(gs ...Ge) Ge { return NewJoiner(gs, ", ", "VALUES", "", true) }
+	Values = func(gs ...Ge) Ge { return NewJoiner(gs, ", ", "VALUES ", "", true) }
 )
+
+// insertBuilder
+/*
+ A standard INSERT :
+ INSERT INTO table (col1, col2) VALUES (?, ?)
+**/
+type insertBuilder struct {
+	table  Ge
+	column []Ge
+	value  []Ge
+}
+
+func InsertBuilder() *insertBuilder {
+	return &insertBuilder{
+		column: []Ge{},
+		value:  []Ge{},
+	}
+}
+
+func (b *insertBuilder) Table(table Ge) *insertBuilder {
+	b.table = table
+	return b
+}
+
+func (b *insertBuilder) Column(columns ...Ge) *insertBuilder {
+	b.column = append(b.column, columns...)
+	return b
+}
+
+func (b *insertBuilder) Value(values ...Ge) *insertBuilder {
+	b.value = append(b.value, values...)
+	return b
+}
+
+func (b *insertBuilder) Clear() *insertBuilder {
+	b.table = nil
+	b.column = []Ge{}
+	b.value = []Ge{}
+	return b
+}
+
+func (b *insertBuilder) SQL() (string, []interface{}) {
+	joiner := NewJoiner([]Ge{Insert(b.table),
+		NewJoiner(b.column, ", ", "", "", true),
+		Values(b.value...)}, " ", "", "", false)
+	return joiner.SQL()
+}
+
+func (b *insertBuilder) Build() (string, []interface{}) {
+	return b.SQL()
+}
--- insert_test.go
+++ insert_test.go
@@ -9,7 +9,15 @@ func TestInsert(t *testing.T) {
 }
 
 func TestValues(t *testing.T) {
-	TestEqual(t, Values(Arg(100)), `VALUES(?)`, []interface{}{100})
-	TestEqual(t, Values(Arg(100), Arg(200)), `VALUES(?, ?)`, []interface{}{100, 200})
-	TestEqual(t, Values(Arg(100), Arg(200), Arg(300)), `VALUES(?, ?, ?)`, []interface{}{100, 200, 300})
+	TestEqual(t, Values(Arg(100)), `VALUES (?)`, []interface{}{100})
+	TestEqual(t, Values(Arg(100), Arg(200)), `VALUES (?, ?)`, []interface{}{100, 200})
+	TestEqual(t, Values(Arg(100), Arg(200), Arg(300)), `VALUES (?, ?, ?)`, []interface{}{100, 200, 300})
+}
+
+func TestInsertBuilder(t *testing.T) {
+	builder := InsertBuilder().
+		Table(T("table_person")).
+		Column(C("col1"), C("col2")).
+		Value(Arg(100), Arg(200))
+	TestEqual(t, builder, `INSERT INTO table_person (col1, col2) VALUES (?, ?)`, []interface{}{100, 200})
 }
