8613510c8858900371f25554cffbea4bd04a9a68
billcoding <wangmiao07@sina.com>
2021-09-02 13:39:18 +0800

    Update: move files

--- select_test.go
+++ select_test.go
@@ -92,3 +92,13 @@ func TestHaving(t *testing.T) {
 	TestEqual(t, Having(OrGroup(Instr("a", 1))), "HAVING ((INSTR(a, ?) > 0))", []interface{}{1})
 	TestEqual(t, Having(OrGroup(Instr("a", 1), Instr("b", 100))), "HAVING ((INSTR(a, ?) > 0) OR (INSTR(b, ?) > 0))", []interface{}{1, 100})
 }
+
+func TestSelectBuilder(t *testing.T) {
+	sb := SelectBuilder().
+		Select(C("a"), C("b")).
+		From(T("table_person")).
+		Join(LeftJoin(As(T("table_a"), "ta"), On(C("ta.col1 = tb.col1")))).
+		Where(AndGroup(Eq("a", 100), Eq("b", 200))).
+		OrderBy(DescGroup(C("a"), C("b")))
+	TestEqual(t, sb, `SELECT a, b FROM table_person LEFT JOIN table_a AS ta ON (ta.col1 = tb.col1) WHERE ((a = ?) AND (b = ?)) ORDER BY a DESC, b DESC`, []interface{}{100, 200})
+}
