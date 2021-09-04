1741237d1621150eb69580b9113f529740636379
billcoding <wangmiao07@sina.com>
2021-09-04 19:04:28 +0800

    Add create & test

---
+++ create.go
@@ -0,0 +1,126 @@
+package sgen
+
+import "fmt"
+
+var (
+	// CreateView
+	//
+	// CREATE VIEW vm_hello_world AS
+	// SELECT UUID() AS uuid
+	CreateView = func(view Ge, viewDefinition Ge) Ge {
+		return NewJoiner([]Ge{NewJoiner([]Ge{Create, View, view}, " ", "", "", false), viewDefinition}, " AS ", "", "", false)
+	}
+	// CreateIndex
+	//
+	// CREATE INDEX idx_name ON table(id, name)
+	CreateIndex = func(unique bool, index Ge, table Ge, columns ...Ge) Ge {
+		return NewJoiner([]Ge{
+			NewJoiner([]Ge{
+				Create, map[bool]Ge{true: Unique, false: P("")}[unique], Index, index, P("ON"), table,
+			}, " ", "", "", false),
+			NewJoiner(columns, ", ", " ", "", true),
+		}, "", "", "", false)
+	}
+	// CreateUniqueIndex
+	//
+	// CREATE UNIQUE INDEX idx_name ON table(id, name)
+	CreateUniqueIndex = func(index Ge, table Ge, columns ...Ge) Ge {
+		return CreateIndex(true, index, table, columns...)
+	}
+	// IndexDefinition defines gen SQL like `unique? index idx_name (id, name)`
+	IndexDefinition = func(unique bool, name Ge, columns ...Ge) Ge {
+		return NewJoiner([]Ge{
+			map[bool]Ge{true: Unique, false: P("")}[unique], Index, name,
+			NewJoiner(columns, ", ", "", "", true),
+		}, " ", "", "", false)
+	}
+	// ColumnDefinition defines gen SQL like `name varchar(50) not null default '000' comment 'Name'`
+	ColumnDefinition = func(name Ge, typ Ge, null, autoIncrement, defaultSet bool, defaultVal, comment string) Ge {
+		return NewJoiner([]Ge{
+			name,
+			typ,
+			map[bool]Ge{true: Null, false: NotNull}[null],
+			map[bool]Ge{true: Default(P(fmt.Sprintf("%s", defaultVal))), false: P("")}[defaultSet],
+			map[bool]Ge{true: AutoIncrement, false: P("")}[autoIncrement],
+			Comment(comment),
+		}, " ", "", "", false)
+	}
+	// PrimaryKey defines gen SQL like `PRIMARY KEY(id, name)`
+	PrimaryKey = func(gs ...Ge) Ge { return NewJoiner(gs, ", ", "PRIMARY KEY ", "", true) }
+	// Default defines gen SQL like `DEFAULT '0'`
+	Default = func(ge Ge) Ge { return NewJoiner([]Ge{P("DEFAULT '"), ge}, "", "", "'", false) }
+	// Comment defines gen SQL like `COMMENT 'comment'`
+	Comment = func(comment string) Ge { return P(fmt.Sprintf("COMMENT '%s'", comment)) }
+)
+
+// createTableBuilder
+//
+// 1.
+// create table table_name(
+// 	id int not null auto_increment comment 'ID',
+// 	name varchar(200) not null default '0' comment 'NAME',
+// 	primary key(id)
+// );
+//
+// 2.
+// create table table_name(
+// 	id int not null auto_increment primary key comment 'ID',
+// 	name varchar(200) not null default '0' comment 'NAME'
+// );
+type createTableBuilder struct {
+	table            Ge
+	columnDefinition []Ge
+	primaryKey       []Ge
+	indexes          []Ge
+}
+
+func CreateTableBuilder() *createTableBuilder {
+	return &createTableBuilder{
+		columnDefinition: []Ge{},
+		primaryKey:       []Ge{},
+		indexes:          []Ge{}}
+}
+
+func (c *createTableBuilder) Table(table Ge) *createTableBuilder {
+	c.table = table
+	return c
+}
+
+func (c *createTableBuilder) ColumnDefinition(columnDefinition ...Ge) *createTableBuilder {
+	c.columnDefinition = append(c.columnDefinition, columnDefinition...)
+	return c
+}
+
+func (c *createTableBuilder) PrimaryKey(columns ...Ge) *createTableBuilder {
+	c.primaryKey = append(c.primaryKey, columns...)
+	return c
+}
+
+func (c *createTableBuilder) Index(index ...Ge) *createTableBuilder {
+	c.indexes = append(c.indexes, index...)
+	return c
+}
+
+func (c *createTableBuilder) Build() (string, []interface{}) {
+	return c.SQL()
+}
+
+func (c *createTableBuilder) Clear() *createTableBuilder {
+	c.table = nil
+	c.columnDefinition = []Ge{}
+	c.primaryKey = []Ge{}
+	c.indexes = []Ge{}
+	return c
+}
+
+func (c *createTableBuilder) SQL() (string, []interface{}) {
+	joiner := NewJoiner([]Ge{
+		NewJoiner([]Ge{P("CREATE"), P("TABLE"), c.table}, " ", "", "", false),
+		NewJoiner([]Ge{
+			NewJoiner(c.columnDefinition, ", ", "", "", false),
+			PrimaryKey(c.primaryKey...),
+			NewJoiner(c.indexes, ", ", "", "", false),
+		}, ", ", "", "", true,
+		)}, "", "", "", false)
+	return joiner.SQL()
+}
---
+++ create_test.go
@@ -0,0 +1,69 @@
+package sgen
+
+import "testing"
+
+func TestCreateView(t *testing.T) {
+	testEqualWithoutPs(t, CreateView(P("vm_nowTime"), P(`select NOW() AS t`)),
+		`CREATE VIEW vm_nowTime AS select NOW() AS t`)
+}
+
+func TestCreateIndex(t *testing.T) {
+	testEqualWithoutPs(t, CreateIndex(false, P("idx_name"), T("table"), C("name")),
+		`CREATE INDEX idx_name ON table (name)`)
+	testEqualWithoutPs(t, CreateIndex(true, P("idx_name"), T("table"), C("name")),
+		`CREATE UNIQUE INDEX idx_name ON table (name)`)
+}
+
+func TestCreateUniqueIndex(t *testing.T) {
+	testEqualWithoutPs(t, CreateUniqueIndex(P("idx_name"), T("table"), C("name")),
+		`CREATE UNIQUE INDEX idx_name ON table (name)`)
+}
+
+func TestIndexDefinition(t *testing.T) {
+	testEqualWithoutPs(t, IndexDefinition(false, P("idx_name"), C("name")),
+		`INDEX idx_name (name)`)
+	testEqualWithoutPs(t, IndexDefinition(false, P("idx_name"), C("name"), C("name1"), C("name2")),
+		`INDEX idx_name (name, name1, name2)`)
+	testEqualWithoutPs(t, IndexDefinition(true, P("idx_name"), C("name")),
+		`UNIQUE INDEX idx_name (name)`)
+	testEqualWithoutPs(t, IndexDefinition(true, P("idx_name"), C("name"), C("name1"), C("name2")),
+		`UNIQUE INDEX idx_name (name, name1, name2)`)
+}
+
+func TestColumnDefinition(t *testing.T) {
+	testEqualWithoutPs(t, ColumnDefinition(P("id"), P("int"), false, true, false, "", "ID"),
+		`id int NOT NULL AUTO_INCREMENT COMMENT 'ID'`)
+	testEqualWithoutPs(t, ColumnDefinition(P("id"), P("bigint"), false, true, false, "", "ID"),
+		`id bigint NOT NULL AUTO_INCREMENT COMMENT 'ID'`)
+	testEqualWithoutPs(t, ColumnDefinition(P("name"), P("varchar(50)"), false, false, false, "", "Name"),
+		`name varchar(50) NOT NULL COMMENT 'Name'`)
+	testEqualWithoutPs(t, ColumnDefinition(P("name"), P("varchar(50)"), true, false, true, "000", "Name"),
+		`name varchar(50) NULL DEFAULT '000' COMMENT 'Name'`)
+}
+
+func TestPrimaryKey(t *testing.T) {
+	testEqualWithoutPs(t, PrimaryKey(C("id")), `PRIMARY KEY (id)`)
+	testEqualWithoutPs(t, PrimaryKey(C("id"), C("name")), `PRIMARY KEY (id, name)`)
+	testEqualWithoutPs(t, PrimaryKey(C("id"), C("name"), C("age")), `PRIMARY KEY (id, name, age)`)
+}
+
+func TestDefault(t *testing.T) {
+	testEqualWithoutPs(t, Default(C("1")), `DEFAULT '1'`)
+	testEqualWithoutPs(t, Default(C("HELP")), `DEFAULT 'HELP'`)
+}
+
+func TestComment(t *testing.T) {
+	testEqualWithoutPs(t, Comment("help for comment"), `COMMENT 'help for comment'`)
+}
+
+func TestCreateTableBuilder(t *testing.T) {
+	builder := CreateTableBuilder().
+		Table(T("table_person")).
+		ColumnDefinition(
+			ColumnDefinition(P("id"), P("int"), false, true, false, "", "ID"),
+			ColumnDefinition(P("name"), P("varchar(50)"), false, false, true, "000", "Name"),
+		).
+		PrimaryKey(C("id")).
+		Index(IndexDefinition(false, P("idx_name"), C("name")))
+	testEqualWithoutPs(t, builder, `CREATE TABLE table_person(id int NOT NULL AUTO_INCREMENT COMMENT 'ID', name varchar(50) NOT NULL DEFAULT '000' COMMENT 'Name', PRIMARY KEY (id), INDEX idx_name (name))`)
+}
