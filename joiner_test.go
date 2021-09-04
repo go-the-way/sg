f15a9e5d01e06bff388288d27601eb41b74e1df7
billcoding <wangmiao07@sina.com>
2021-09-04 17:14:32 +0800

    Update: test

--- joiner_test.go
+++ joiner_test.go
@@ -6,23 +6,23 @@ import (
 
 func TestJoiner(t *testing.T) {
 	j := &Joiner{sep: "sep", prefix: "prefix", suffix: "suffix", group: true, append: "append"}
-	testSingleEqual(t, "sep", j.sep)
-	testSingleEqual(t, "prefix", j.prefix)
-	testSingleEqual(t, "suffix", j.suffix)
-	testSingleEqual(t, true, j.group)
-	testSingleEqual(t, "append", j.append)
+	testEqualInterface(t, "sep", j.sep)
+	testEqualInterface(t, "prefix", j.prefix)
+	testEqualInterface(t, "suffix", j.suffix)
+	testEqualInterface(t, true, j.group)
+	testEqualInterface(t, "append", j.append)
 }
 
 func TestNewJoiner(t *testing.T) {
 	j := &Joiner{sep: "sep", prefix: "prefix", suffix: "suffix", group: true}
 	expect := NewJoiner(nil, "sep", "prefix", "suffix", true)
-	testSingleEqual(t, expect, j)
+	testEqualInterface(t, expect, j)
 }
 
 func TestNewJoinerWithAppend(t *testing.T) {
 	j := &Joiner{sep: "sep", prefix: "prefix", suffix: "suffix", group: true, append: "append"}
 	expect := NewJoinerWithAppend(nil, "sep", "prefix", "suffix", "append", true)
-	testSingleEqual(t, expect, j)
+	testEqualInterface(t, expect, j)
 }
 
 func TestJoiner_SQL(t *testing.T) {
--- test_util.go
+++ test_util.go
@@ -12,21 +12,27 @@ func (g *geImpl) SQL() (string, []interface{}) {
 	return "geImpl", nil
 }
 
-func testSingleEqual(t *testing.T, expect, v interface{}) {
+func testEqualInterface(t *testing.T, expect, v interface{}) {
 	if !reflect.DeepEqual(expect, v) {
 		t.Fatalf("%v expect = [%v] but get = [%v]\n", t.Name(), expect, v)
 	}
 	t.Logf("%v expect = [%v] get get = [%v]\n", t.Name(), expect, v)
 }
 
-func testEqual(t *testing.T, g Generator, expectSql string, expectPs []interface{}) {
+func testEqual(t *testing.T, g Ge, expectSql string, expectPs []interface{}) {
 	getSql, getPs := g.SQL()
 	if getSql != expectSql {
 		t.Fatalf("%v expect sql = [%v] but get sql = [%v]\n", t.Name(), expectSql, getSql)
 	}
-	if !reflect.DeepEqual(getPs, expectPs) {
-		t.Fatalf("%v expect ps = [%v] but get ps = [%v]\n", t.Name(), expectPs, getPs)
-	}
 	t.Logf("%v expect sql = [%v] get sql = [%v]\n", t.Name(), expectSql, getSql)
-	t.Logf("%v expect ps = [%v] get ps = [%v]\n", t.Name(), expectPs, getPs)
+	if expectPs != nil {
+		if !reflect.DeepEqual(getPs, expectPs) {
+			t.Fatalf("%v expect ps = [%v] but get ps = [%v]\n", t.Name(), expectPs, getPs)
+		}
+		t.Logf("%v expect ps = [%v] get ps = [%v]\n", t.Name(), expectPs, getPs)
+	}
+}
+
+func testEqualWithoutPs(t *testing.T, g Ge, expectSql string) {
+	testEqual(t, g, expectSql, nil)
 }
