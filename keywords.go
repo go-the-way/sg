808ad7cf7a7a7dd028be4570e4ccb39a33db0974
billcoding <wangmiao07@sina.com>
2021-09-04 19:05:05 +0800

    Add keywords & test

--- keywords.go
+++ keywords.go
@@ -1,11 +1,15 @@
 package sgen
 
 const (
-	Table     = P("TABLE")
-	Drop      = P("DROP")
-	View      = P("VIEW")
-	Event     = P("EVENT")
-	Procedure = P("PROCEDURE")
-	NotNull   = P("NOT NULL")
-	Null      = P("NULL")
+	Table         = P("TABLE")
+	Create        = P("CREATE")
+	Drop          = P("DROP")
+	View          = P("VIEW")
+	Unique        = P("UNIQUE")
+	Index         = P("INDEX")
+	Event         = P("EVENT")
+	Procedure     = P("PROCEDURE")
+	AutoIncrement = P("AUTO_INCREMENT")
+	NotNull       = P("NOT NULL")
+	Null          = P("NULL")
 )
--- keywords_test.go
+++ keywords_test.go
@@ -3,11 +3,15 @@ package sgen
 import "testing"
 
 func TestKeywords(t *testing.T) {
-	testEqualWithoutPs(t, Table, `TABLE`)
-	testEqualWithoutPs(t, Drop, `DROP`)
-	testEqualWithoutPs(t, View, `VIEW`)
-	testEqualWithoutPs(t, Event, `EVENT`)
-	testEqualWithoutPs(t, Procedure, `PROCEDURE`)
-	testEqualWithoutPs(t, NotNull, `NOT NULL`)
-	testEqualWithoutPs(t, Null, `NULL`)
+	testEqualWithoutPs(t, Table, "TABLE")
+	testEqualWithoutPs(t, Create, "CREATE")
+	testEqualWithoutPs(t, Drop, "DROP")
+	testEqualWithoutPs(t, View, "VIEW")
+	testEqualWithoutPs(t, Unique, "UNIQUE")
+	testEqualWithoutPs(t, Index, "INDEX")
+	testEqualWithoutPs(t, Event, "EVENT")
+	testEqualWithoutPs(t, Procedure, "PROCEDURE")
+	testEqualWithoutPs(t, AutoIncrement, "AUTO_INCREMENT")
+	testEqualWithoutPs(t, NotNull, "NOT NULL")
+	testEqualWithoutPs(t, Null, "NULL")
 }
