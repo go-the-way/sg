2c2adc4990e35c49047d3cc0069834215bca1123
billcoding <wangmiao07@sina.com>
2021-09-04 19:05:54 +0800

    Update: test_util.go

--- test_util.go
+++ test_util.go
@@ -14,22 +14,22 @@ func (g *geImpl) SQL() (string, []interface{}) {
 
 func testEqualInterface(t *testing.T, expect, v interface{}) {
 	if !reflect.DeepEqual(expect, v) {
-		t.Fatalf("%v expect = [%v] but get = [%v]\n", t.Name(), expect, v)
+		t.Fatalf("%v expect = [%v] \n    get = [%v]\n", t.Name(), expect, v)
 	}
-	t.Logf("%v expect = [%v] get get = [%v]\n", t.Name(), expect, v)
+	t.Logf("%v expect = [%v] \nget    get = [%v]\n", t.Name(), expect, v)
 }
 
 func testEqual(t *testing.T, g Ge, expectSql string, expectPs []interface{}) {
 	getSql, getPs := g.SQL()
 	if getSql != expectSql {
-		t.Fatalf("%v expect sql = [%v] but get sql = [%v]\n", t.Name(), expectSql, getSql)
+		t.Fatalf("%v \nexpect sql = [%v] \nget    sql = [%v]\n", t.Name(), expectSql, getSql)
 	}
-	t.Logf("%v expect sql = [%v] get sql = [%v]\n", t.Name(), expectSql, getSql)
+	t.Logf("%v \nexpect sql = [%v] \nget    sql = [%v]\n", t.Name(), expectSql, getSql)
 	if expectPs != nil {
 		if !reflect.DeepEqual(getPs, expectPs) {
-			t.Fatalf("%v expect ps = [%v] but get ps = [%v]\n", t.Name(), expectPs, getPs)
+			t.Fatalf("%v \nexpect ps = [%v] \nget    ps = [%v]\n", t.Name(), expectPs, getPs)
 		}
-		t.Logf("%v expect ps = [%v] get ps = [%v]\n", t.Name(), expectPs, getPs)
+		t.Logf("%v \nexpect ps = [%v] \nget    ps = [%v]\n", t.Name(), expectPs, getPs)
 	}
 }
 
